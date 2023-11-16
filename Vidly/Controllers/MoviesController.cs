065836fe7a295056fe48f58470f05cd1de83c75d
Sahil Shrestha <35665722+sahilian@users.noreply.github.com>
2023-11-16 23:08:44 +0545

    Proper Body

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        //GET: /Movies/Random
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Avatar" };
+            var customers = new List<Customer>()
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            /*ViewData["Movie"] = movie;*/
+            return View(viewModel);
+        }
+
+        /*public IActionResult Edit(int Id)
+        {
+            return Content("Id=" + Id);
+        }
+
+        public IActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(string.Format("pageIndex={0} & sortBy={1}", pageIndex, sortBy));
+        }*/
+
+        [Route("movies/released/{year:regex(\\d{{4}})}/{month:regex(\\d{{2}}):range(1,12)}")]
+        public IActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
--- Vidly/Program.cs
+++ Vidly/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -20,6 +22,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+/*app.MapControllerRoute(
+    "MoviesByReleaseDate",
+    "movies/released/{year}/{month}",
+    new {controller= "Movies", action= "ByReleaseDate"},
+    new { year = @"2015|2016", month = @"\d{2}" });*/
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
